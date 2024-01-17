0777ffccc89326dea745b341f2ad34acc8242a7a
unknown <benguiratoussama1998@gmail.com>
2024-01-17 20:47:11 +0100

    shortest sentence functionnality failed

--- script.js
+++ script.js
@@ -110,5 +110,21 @@ textArea.addEventListener("input", function() {
 
 //details shortest sentence display
 textArea.addEventListener("input", function() {
-
+  let mainCounter = 1;
+  let splitedSentence = textArea.value.split(".");
+  let splitedWords = splitedSentence.map(item => {
+    return item.split(" ");
+  })
+  for(let i = 0; i < splitedWords.length; i++) {
+    let innerCounter = 0;
+    for(let j = 0; j < splitedWords[i].length; j++) {
+      if(splitedWords[i][j] != "") {
+        innerCounter++;
+      }
+    }
+    if(innerCounter < mainCounter) {
+      mainCounter = innerCounter;
+    }
+  }
+  shortestSentence.textContent = mainCounter;
 });

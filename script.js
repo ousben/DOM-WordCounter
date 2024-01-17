a1adf33f93fa55fa0ea74a79970e132fb637ee58
unknown <benguiratoussama1998@gmail.com>
2024-01-17 15:55:06 +0100

    JS logic and other features

---
+++ script.js
@@ -0,0 +1,24 @@
+//first div
+const resultWords = document.getElementById("result-words");
+const resultCharacters = document.getElementById("result-characters");
+
+//second div
+const textArea = document.getElementById("text-area");
+
+//third div
+const words = document.getElementById("words");
+const uniqueWords = document.getElementById("unique-words");
+const characters = document.getElementById("characters");
+const sentences = document.getElementById("sentences");
+const longuestSentence = document.getElementById("longuest-sentence");
+const shortestSentence = document.getElementById("shortest-sentence");
+const averageSentenceWords = document.getElementById("average-sentence-words");
+const averageSentenceChars = document.getElementById("average-sentence-chars");
+const averageWordLength = document.getElementById("average-word-length");
+
+//fourth div
+const densityList = document.getElementById("density-list");
+
+textArea.addEventListener("input", function() {
+  console.log(textArea.value);
+})
